2445f0784809ae5ce6073cbb57b6a19d0b79704d
Yurii Kleban <yurakleban@gmail.com>
2026-02-01 14:52:38 +0200

    DI added

---
+++ AaeIcs.Client/Services/AppConfigService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace AaeIcs.Client.Services
+{
+    public class AppConfigService : IAppConfigService
+    {
+        public IConfiguration Configuration { get; }
+
+        public AppConfigService()
+        {
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .Build();
+        }
+
+        public string? Get(string key) => Configuration[key];
+    }
+}
---
+++ AaeIcs.Client/Services/IAppConfigService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AaeIcs.Client.Services
+{
+    public interface IAppConfigService
+    {
+        IConfiguration Configuration { get; }
+        string? Get(string key);
+    }
+}
